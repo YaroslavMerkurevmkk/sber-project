798f1fbc60ad5df778d2c29c6b99d2c2135fc929
DartCat25 <vlaadmai005@gmail.com>
2025-09-21 21:29:56 +0300

    [front/templates] Built new index

--- src/front/static/front/js/chat.js
+++ src/front/static/front/js/chat.js
@@ -1,4 +1,20 @@
-const roomName = JSON.parse(document.getElementById('room-name').textContent);
+// //Interface
+
+const roomName = JSON.parse(document.getElementById('chatname').innerHTML);
+
+const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/chat/');
+
+chatSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    if (data.type === "init") {
+        const chatsDiv = document.getElementById("chats");
+        data.chats.forEach(chat => {
+            const el = document.createElement("div");
+            el.innerHTML = el.title = `Chat: ${chat.name} (ID: ${chat.id})`;
+            chatsDiv.appendChild(el);
+        });
+    }
+};
 
 const chatSocket = new WebSocket(
     'ws://'
@@ -17,8 +33,8 @@ chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
 };
 
-document.querySelector('#chat-message-input').focus();
-document.querySelector('#chat-message-input').onkeyup = function(e) {
+document.querySelector('#chat-input').focus();
+document.querySelector('#chat-input').onkeyup = function(e) {
     if (e.keyCode === 13) {  // enter, return
         document.querySelector('#chat-message-submit').click();
     }
@@ -32,3 +48,66 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
     }));
     messageInputDom.value = '';
 };
+
+// // Exit button warning
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.getElementById('exit-button').addEventListener('click', () => {
+        Swal.fire({
+            title: "Выход",
+            text: "Вы уверены?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '<a href="/logout">Да</a>',
+            cancelButtonText: "Отмена",
+        })
+    });
+});
+
+// // Load chats
+
+const chatListSocket = new WebSocket('ws://' + window.location.host + '/ws/chat/');
+
+chatListSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    console.log(data);
+    if (data.type === "data" && data.payload && "chats" in data.payload) {
+        const chatsDiv = document.getElementById("chats");
+        data.payload.chats.forEach(chat => {
+            const el = document.createElement("div");
+            el.textContent = `Chat: ${chat.name} (ID: ${chat.id})`;
+            chatsDiv.appendChild(el);
+        });
+    }
+    if (data.type === "notification") {
+        Swal.fire("Notification", data.payload.message, data.payload.result);
+    }
+};
+
+// // Create chat
+
+document.getElementById("create-chat-btn").addEventListener("click", () => {
+    Swal.fire({
+        title: "Создать новый чат",
+        input: "text",
+        inputLabel: "Введите название",
+        showCancelButton: true,
+        confirmButtonText: "Создать",
+        cancelButtonText: "Отмена",
+        inputValidator: (value) => {
+            if (!value) {
+                return "Название не может быть пустым!";
+            }
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            chatSocket.send(JSON.stringify({
+                type: "command",
+                payload: {
+                    action: "create_chat",
+                    name: result.value
+                }
+            }));
+        }
+    });
+});
